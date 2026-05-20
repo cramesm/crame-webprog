71ae74ab4dcb48e42a81013f7d59b47c13b426a4
cramesm <cramesm@students.national-u.edu.ph>
2026-05-20 17:03:50 +0800

    fix: restore production MongoDB URI and correct User.find typo

--- crame-server/controllers/personnelController.js
+++ crame-server/controllers/personnelController.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 
 exports.getUsers = async (req, res) => {
   try {
-    const users = await User.find({}, '-password'); 
+    const users = await User.find({}, '-password');
     res.json({ users });
   } catch (error) {
     res.status(500).json({ message: error.message });
